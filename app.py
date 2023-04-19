93047081d862b3623d9ff69eda0eb16b41f7e478
himanshu0072 <himanshu2003prajapati@gmail.com>
2023-04-19 17:17:17 +0530

    Login and registration page is created

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from werkzeug .utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def form_view():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        gender = request.form['gender']
+        print(name)
+        print(email)
+        print(password)
+        print(gender)
+    return render_template('user_registration.html')
+
+
+@app.route('/login')
+def func_name():
+    return render_template('user_login.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
